3130c1843cd351573810f03dbed917395d4d118d
MaybeYuuki <baseballff13@gmail.com>
2022-08-03 20:12:08 +0900

    自動生成の制約の追加

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     encrypted_password    { password }
     first_name            { '田中' }
     family_name           { '太郎' }
