13fed9c8de8594bf10d3f20d8ddc16ba902fb3e5
MaybeYuuki <baseballff13@gmail.com>
2022-08-03 15:04:42 +0900

    ユーザー登録の条件の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
   validates :birth_date, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
+    validates :first_name, presence: true
+    validates :family_name, presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+    validates :first_name_kana, presence: true
+    validates :family_name_kana, presence: true
+  end
+
+  
+  validates :email, presence: true, format: { with: /\A\S+@\S+\.\S+\z/, message: "can't be blank" }
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and number' }
 end
