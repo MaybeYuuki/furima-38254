f50c8809ad710f0446f2a5971cbad78365c4e382
MaybeYuuki <baseballff13@gmail.com>
2022-08-03 17:13:36 +0900

    nameの名称変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :nickname, presence: true
   validates :birth_date, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
-    validates :first_name, presence: true
-    validates :family_name, presence: true
+    validates :first_name
+    validates :family_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
-    validates :first_name_kana, presence: true
-    validates :family_name_kana, presence: true
+    validates :first_name_kana
+    validates :family_name_kana
   end
 
   
