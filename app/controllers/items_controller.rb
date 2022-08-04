26824bee07dfbfc296a4a7de83b3cdd2754d1dd9
MaybeYuuki <baseballff13@gmail.com>
2022-08-04 19:00:05 +0900

    カテゴリーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:price,:postage_id,:category_id,:explanation,:status_id,:area_id,:days_id,)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :postage_id
+    validates :explanation
+    validates :status_id
+    validates :area_id
+    validates :days_id
+    validates :category_id
+  end
 end
