c8d8378d7c7843abfebddfb3c5717719cb11969e
MaybeYuuki <baseballff13@gmail.com>
2022-08-05 14:51:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,22 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :area
+  belongs_to :postage
+  belongs_to :days
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :status_id
+    validates :area_id
+    validates :postage_id
+    validates :days_id
+  end
+
   belongs_to :user
 
   with_options presence: true do
