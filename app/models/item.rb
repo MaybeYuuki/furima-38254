51ddf2bbb586dc70336967ca55b8283df54f9a29
MaybeYuuki <baseballff13@gmail.com>
2022-08-05 23:30:25 +0900

    保存しないプルダウンの訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
 
-  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+  with_options numericality: { other_than: 0 , message: "can't be blank"} do
     validates :category_id
     validates :status_id
     validates :area_id
