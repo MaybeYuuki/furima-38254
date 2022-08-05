5754f4bb329d458eba31b5f1cd35ca57448db5f6
MaybeYuuki <baseballff13@gmail.com>
2022-08-05 18:26:28 +0900

    販売利益の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,17 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+function price() {
+  const priceInput = document.getElementById("item-price");
+  const add_tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  priceInput.addEventListener("keyup", () => {
+    const value = priceInput.value;
+
+    if (value >= 300 && value <= 9999999) {
+      add_tax.innerHTML = Math.floor(value * 0.1);
+      profit.innerHTML = Math.floor(value - (value * 0.1));
+    } else {
+      add_tax.innerHTML = "";
+      profit.innerHTML = "";
+    }
+  });
+}
+window.addEventListener("load", price);
