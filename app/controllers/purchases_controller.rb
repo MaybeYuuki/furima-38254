acbbc9aa76e3b1d8c15e7b393ec7800f93f970c9
MaybeYuuki <baseballff13@gmail.com>
2022-08-15 21:36:34 +0900

    Formオブジェクトのインスタンス生成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,22 @@
 class PurchasesController < ApplicationController
   def index
+    @purchase = PurchaseAddress.new
   end
 
   def create
+    @purchase = PurchaseAddress.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :area_id, :municipality, :building_name, :address, :house_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
