f7f610036d9aadd1b93b001cd1d28003cc2a71e9
MaybeYuuki <baseballff13@gmail.com>
2022-08-16 16:51:46 +0900

    カラムの型の変更

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2022_08_15_055318) do
     t.string "municipalities", null: false
     t.string "address", null: false
     t.string "building_name"
-    t.integer "phone_number", null: false
+    t.string "phone_number", null: false
     t.bigint "purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
