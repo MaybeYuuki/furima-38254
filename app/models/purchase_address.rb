204ba07d62c8a6732a322151ed3d159ccbb33010
MaybeYuuki <baseballff13@gmail.com>
2022-08-16 16:52:49 +0900

    モデルの記述ミスの訂正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,13 +8,13 @@ class PurchaseAddress
     validates :municipalities
     validates :address
     validates :token
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A[0-9]{10.11}\z/, message: 'is invalid' }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
   validates :area_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, area_id: area_id, municipalities: municipalities, phone_number: phone_number, building_name: building_name, address: :address)
+    Address.create(postal_code: postal_code, area_id: area_id, municipalities: municipalities, phone_number: phone_number, building_name: building_name, address: address, purchase_id: purchase.id)
   end
 end
