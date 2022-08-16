f9ff40c8a4155b9d00b14d1e92458147656ebfb3
MaybeYuuki <baseballff13@gmail.com>
2022-08-16 15:18:42 +0900

    バリデーションの訂正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,16 +1,20 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :area_id, :municipalities, :address, :building_name, :phone_number, :price, :user_id
+  attr_accessor :postal_code, :area_id, :municipalities, :address, :building_name, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
     validates :user_id
+    validates :item_id
+    validates :municipalities
+    validates :address
+    validates :token
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :area_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    purchase = Purchase.create(price: price, user_id: user_id)
-    Address.create(postal_code: postal_code, area_id: area.id, municipalities: municipalities, house_number: house_number, building_name: building_name, address: :address)
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, area_id: area_id, municipalities: municipalities, phone_number: phone_number, building_name: building_name, address: :address)
   end
 end
