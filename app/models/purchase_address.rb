db9252ed93c24fc161afd015b8b8f9a20a623faf
MaybeYuuki <baseballff13@gmail.com>
2022-08-17 14:48:17 +0900

    バリデーションの訂正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,10 +8,10 @@ class PurchaseAddress
     validates :municipalities
     validates :address
     validates :token
-    validates :phone_number, format: { with: /\A[0-9]{10.11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
-  validates :area_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :area_id, numericality: {other_than: 0, message: "select"}
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
