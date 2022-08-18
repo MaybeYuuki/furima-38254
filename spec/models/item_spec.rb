fc297bab9f76f7208fb4c437aa2814c7e1a97ec2
MaybeYuuki <baseballff13@gmail.com>
2022-08-18 17:34:08 +0900

    商品出品のエラーメッセージの日本語化の単体テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,73 +14,73 @@ RSpec.describe Item, type: :model do
     it '画像がないと登録できない' do
       @item.image = nil
       @item.valid?
-      expect(@item.errors.full_messages).to include("Image can't be blank")
+      expect(@item.errors.full_messages).to include("画像を入力してください")
     end
 
     it '商品名がないと登録できない' do
       @item.name = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Name can't be blank")
+      expect(@item.errors.full_messages).to include("商品名を入力してください")
     end
 
     it '商品の説明がないと登録できない' do
       @item.explanation = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      expect(@item.errors.full_messages).to include("商品の説明を入力してください")
     end
 
     it 'カテゴリーのプルダウンが---だと登録できない' do
       @item.category_id = 0
       @item.valid?
-      expect(@item.errors.full_messages).to include('Category select')
+      expect(@item.errors.full_messages).to include('カテゴリー選択して下さい')
     end
 
     it '商品の状態についてのプルダウンが---だと登録できない' do
       @item.status_id = 0
       @item.valid?
-      expect(@item.errors.full_messages).to include('Status select')
+      expect(@item.errors.full_messages).to include('商品の状態選択して下さい')
     end
 
     it '配送料の負担についてのプルダウンが---だと登録できない' do
       @item.postage_id = 0
       @item.valid?
-      expect(@item.errors.full_messages).to include('Postage select')
+      expect(@item.errors.full_messages).to include('送料の負担選択して下さい')
     end
 
     it '発送元の地域についてのプルダウンが---だと登録できない' do
       @item.area_id = 0
       @item.valid?
-      expect(@item.errors.full_messages).to include('Area select')
+      expect(@item.errors.full_messages).to include('発送元の地域選択して下さい')
     end
 
     it '発送までの日数についてのプルダウンが---だと登録できない' do
       @item.days_id = 0
       @item.valid?
-      expect(@item.errors.full_messages).to include('Days select')
+      expect(@item.errors.full_messages).to include('発送までの日付選択して下さい')
     end
 
     it '価格についての情報がないと登録できない' do
       @item.price = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank")
+      expect(@item.errors.full_messages).to include("販売価格を入力してください")
     end
 
     it '価格が300円以下だと登録できない' do
       @item.price = 299
       @item.valid?
-      expect(@item.errors.full_messages).to include('Price Out of setting range')
+      expect(@item.errors.full_messages).to include('販売価格の制限金額以内で入力して下さい')
     end
 
     it '価格が9999999円以上だと登録できない' do
       @item.price = 10_000_000
       @item.valid?
-      expect(@item.errors.full_messages).to include('Price Out of setting range')
+      expect(@item.errors.full_messages).to include('販売価格の制限金額以内で入力して下さい')
     end
 
     it '販売価格は半角数字以外では登録できない' do
       @item.price = '１０００'
       @item.valid?
-      expect(@item.errors.full_messages).to include('Price Out of setting range')
+      expect(@item.errors.full_messages).to include('販売価格の制限金額以内で入力して下さい')
     end
 
     it 'userが紐づいていないと保存できない' do
