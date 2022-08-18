6d68733875a87e3bbbcce2529f4f77e780db0a60
MaybeYuuki <baseballff13@gmail.com>
2022-08-18 18:19:18 +0900

    コントローラーの訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :require_login, only: :new, alert: 'You need to sign in or sign up before continuing.'
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
