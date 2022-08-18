ee9bc58c6dce20653b7e5cd17e6d349b1943a46a
MaybeYuuki <baseballff13@gmail.com>
2022-08-18 17:31:49 +0900

    新規登録のエラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,18 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birth_date, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: 'Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: '全角で入力して下さい' } do
     validates :first_name
     validates :family_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' } do
     validates :first_name_kana
     validates :family_name_kana
   end
 
-  validates :email, presence: true, format: { with: /\A\S+@\S+\.\S+\z/, message: "can't be blank" }
+  validates :email, presence: true, format: { with: /\A\S+@\S+\.\S+\z/ }
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and number' }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: '文字と数字の両方を含めて下さい' }
 end
